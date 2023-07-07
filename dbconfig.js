c4867003443a6eedd5fb999af5c9fb342021645f
Balamurugan <msbalacse@gmail.com>
2023-07-07 12:27:37 +0530

    updated

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const connection = process.env.POSTGRES_URL;
+const connection = process.env.POSTGRES_URL + "?sslmode=require";
 
 const pool = new Pool({
   connectionString: connection,
